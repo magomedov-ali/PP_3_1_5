e4e913350f0a112fd89f1d5f0a1f4d00e3b5140b
magomedov-ali <magomedov.ali.2000@gmail.com>
2024-02-09 02:28:51 +0300

    first commit for 3.1.4. no edit and delete modals. no authorization

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,16 +1,15 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.RegistrationService;
 import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/admin")
@@ -28,40 +27,35 @@ public class AdminController {
     }
 
     @GetMapping()
-    public String index(Model model) {
+    public String index(@AuthenticationPrincipal UserDetails userDetails, @ModelAttribute("newUser") User newUser, Model model) {
+        User authorizedUser = userService.findByUsername(userDetails.getUsername());
         model.addAttribute("users", userService.findAllFetchRoles());
-        return "users/index";
+        model.addAttribute("authorizedUser", authorizedUser);
+        model.addAttribute("allRoles", roleService.findAll());
+        return "mainPage";
     }
 
-    @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user, Model model) {
-        model.addAttribute("all_roles", roleService.findAll());
-        return "users/new";
-    }
+//    @GetMapping("/new")
+//    public String newUser(@ModelAttribute("user") User user, Model model) {
+//        model.addAttribute("all_roles", roleService.findAll());
+//        return "users/new";
+//    }
 
     @PostMapping()
-    public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("all_roles", roleService.findAll());
-            return "users/new";
-        }
+    public String create(@ModelAttribute("user") User user) {
         registrationService.register(user);
         return "redirect:/admin";
     }
 
-    @PostMapping("/edit")
-    public String edit(@RequestParam("id") int id, Model model) {
-        model.addAttribute("user", userService.findOne(id));
-        model.addAttribute("all_roles", roleService.findAll());
-        return "users/edit";
-    }
+//    @PostMapping("/edit")
+//    public String edit(@RequestParam("id") int id, Model model) {
+//        model.addAttribute("user", userService.findOne(id));
+//        model.addAttribute("all_roles", roleService.findAll());
+//        return "users/edit";
+//    }
 
     @PatchMapping("/update")
-    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, @RequestParam("id") int id, Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("all_roles", roleService.findAll());
-            return "users/edit";
-        }
+    public String update(@ModelAttribute("user") User user, @RequestParam("id") int id) {
         registrationService.update(id, user);
         return "redirect:/admin";
     }
